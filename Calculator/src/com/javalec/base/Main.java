61a25c069ec6a5be2b0e5da2b273203c90909e0a
정주영 <jjy4922@naver.com>
2021-09-08 16:10:58 +0900

    second main commit

--- Calculator/src/com/javalec/base/Main.java
+++ Calculator/src/com/javalec/base/Main.java
@@ -1,10 +1,28 @@
 package com.javalec.base;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		Scanner scanner = new Scanner(System.in);
+		int num1 = 0;
+		int num2 = 0;
+	
+		System.out.println("첫번쨰 번호를 입력하세요: ");
+		num1 = scanner.nextInt();
+		System.out.println("두번쨰 번호를 입력하세요: ");
+		num1 = scanner.nextInt();
+		
+		Calcurator calcurator = new Calcurator;
+		System.out.println(">>>>>결과<<<<<<<");
+		
+		calcurator.addcal;
+		calcurator.divcal;
+		calcurator.doucal;
+		calcurator.mincal;
+		
 	}
 
 }
